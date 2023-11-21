31ec53ec1cbd942137a505215c3b25c2ede85558
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2023-11-20 20:20:00 -0400

    Dawn of a new life

--- ShaderFlow/Modules/SombreroContext.py
+++
@@ -1,7 +0,0 @@
-from . import *
-
-
-@attrs.define
-class SombreroContext(SombreroModule):
-    width:  int = 1920
-    height: int = 1080
--- ShaderFlow/Modules/__init__.py
+++
@@ -1,2 +0,0 @@
-from .. import *
-from .SombreroContext import *
