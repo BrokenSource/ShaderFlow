757227f44270dc978a6db7ad577f4e33f7ce8011
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2026-02-08 04:11:57 -0300

    Move proper environment setdefaults here

--- shaderflow/__init__.py
+++ shaderflow/__init__.py
@@ -1,9 +1,20 @@
 from dearlog import logger # isort: split
 
+import os
 import importlib.metadata
 
 __version__: str = importlib.metadata.version(__package__)
 __about__:   str = "🔥 Modular shader engine designed for simplicity and speed"
+
+# Warn: If using PyTorch CPU, set `torch.set_num_threads(multiprocessing.cpu_count())`
+# Huge CPU usage for little to no speed up on matrix multiplication of NumPy's BLAS
+# - https://github.com/numpy/numpy/issues/18669#issuecomment-820510379
+os.environ.setdefault("OMP_NUM_THREADS", "1")
+
+# Nvidia: Fix cpu usage on glfw.swap_buffers when vsync is off and the gpu is overwhelmed
+# - https://forums.developer.nvidia.com/t/glxswapbuffers-gobbling-up-a-full-cpu-core-when-vsync-is-off/156635
+# - https://forums.developer.nvidia.com/t/gl-yield-and-performance-issues/27736
+os.environ.setdefault("__GL_YIELD", "USLEEP")
 
 from broken.project import BrokenProject
 
