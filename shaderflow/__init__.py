81a22f0971d4130921e06eb9214b5bb94a4a1a70
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2026-02-19 17:34:22 -0300

    Remove OMP_NUM_THREADS as it seems fixed in recent numpy?

--- shaderflow/__init__.py
+++ shaderflow/__init__.py
@@ -13,11 +13,6 @@ RESOURCES: Path = Path(__file__).parent/"resources"
 
 import os
 
-# Warn: If using PyTorch CPU, set `torch.set_num_threads(multiprocessing.cpu_count())`
-# Huge CPU usage for little to no speed up on matrix multiplication of NumPy's BLAS
-# - https://github.com/numpy/numpy/issues/18669#issuecomment-820510379
-os.environ.setdefault("OMP_NUM_THREADS", "1")
-
 # Nvidia: Fix cpu usage on glfw.swap_buffers when vsync is off and the gpu is overwhelmed
 # - https://forums.developer.nvidia.com/t/glxswapbuffers-gobbling-up-a-full-cpu-core-when-vsync-is-off/156635
 # - https://forums.developer.nvidia.com/t/gl-yield-and-performance-issues/27736
