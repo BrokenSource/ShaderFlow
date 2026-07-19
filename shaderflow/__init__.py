8e260f8a682c4fbc7577bcf96f15e40b66b30c36
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2026-07-19 08:51:09 -0300

    speed: sync init metadata with a script rather than runtime importlib

--- shaderflow/__init__.py
+++ shaderflow/__init__.py
@@ -1,10 +1,9 @@
 from dearlog import logger  # isort: split
 
-from importlib.metadata import metadata
-
-__meta__    = metadata(str(__package__))
-__about__   = __meta__.get("Summary")
-__version__ = __meta__.get("Version")
+__about__   = "🔥 Modular shader engine designed for simplicity and speed"
+__package__ = "shaderflow"
+__version__ = "0.11.1"
+__license__ = "AGPL-3.0"
 
 from pathlib import Path
 
